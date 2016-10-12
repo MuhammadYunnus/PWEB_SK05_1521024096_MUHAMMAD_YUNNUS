2d778d8b53aeaeeae4de6ca0e5edaee4394e880c
MuhammadYunnus <Muhammadyunnuswu@gmail.com>
2016-10-12 16:30:41 +0700

    SK_05

---
+++ 5_2_1521024096_MUHAMMAD_YUNNUS.php
@@ -0,0 +1,27 @@
+<?php
+$X = array
+   (
+	   array("1001","Sabun Lifebuoy","1.500"),
+	   array("1002","Permen Blaster","5.600"),
+	   array("1003","Pasta Gigi Pepsodent","4.560"),
+	   array("1004","Madu Arbain","30.000"),
+	   array("1005","Kecap ABC","7.250"),
+	   array("1006","Saus Tomat ABC","6.700"),
+   	   array("1007","Gula Gulaku","8.900"),
+	   array("1008","Rinso","7.100"),
+	   array("1009","Super Pel","6.450"),
+	   array("1010","Permen Tango","5.600")
+   );
+
+	echo "Kode Barang ". $X[9][0].": Nama Barang: ".$X[9][1].", Harga: ".$X[9][2].".<br>";
+		echo "Kode Barang ". $X[8][0].": Nama Barang: ".$X[8][1].", Harga: ".$X[8][2].".<br>";
+	echo "Kode Barang ". $X[7][0].": Nama Barang: ".$X[7][1].", Harga: ".$X[7][2].".<br>";
+		echo "Kode Barang ". $X[6][0].": Nama Barang: ".$X[6][1].", Harga: ".$X[6][2].".<br>";
+	echo "Kode Barang ". $X[5][0].": Nama Barang: ".$X[5][1].", Harga: ".$X[5][2].".<br>";
+		echo "Kode Barang ". $X[4][0].": Nama Barang: ".$X[4][1].", Harga: ".$X[4][2].".<br>";
+	echo "Kode Barang ". $X[3][0].": Nama Barang: ".$X[3][1].", Harga: ".$X[3][2].".<br>";
+		echo "Kode Barang ". $X[2][0].": Nama Barang: ".$X[2][1].", Harga: ".$X[2][2].".<br>";
+	echo "Kode Barang ". $X[1][0].": Nama Barang: ".$X[1][1].", Harga: ".$X[1][2].".<br>";
+		echo "Kode Barang ". $X[0][0].": Nama Barang: ".$X[0][1].", Harga: ".$X[0][2].".<br>";
+
+?>
